2c9db36441f312940c098c44d705ff0fb97f1132
radwan-77 <radwantareq66@gmail.com>
2025-05-20 16:55:54 +0200

    init some pages

--- app/Models/admin.php
+++ app/Models/admin.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class admin extends Model
 {
-    //
+    // first admin dashboard maxium 10 days 
+    // seconde main actor maxium 10 days 
+    // and then the authication
+    // make 3 folders in resources/views/admin
+    // 1.admin 2.tutor 3.student
+    // and then run the application to see welcome page 
+    // if not using herd uncomment the database in env 
+    // write mysql -u root to open the databsae ( "may be in Quiz" ) 
+    // create database name; 
+    // to check if the database is created
+    // you will 1 row affceted
+    // /q to quit the database "mysql" ( "may be in Quiz" )
+    // and then write php artisan migrate
+    // the quiz will be in the commaned of php atisan
+    // like making a model or controller as well as simple function like index 
+    // and then run the application to see welcome page  
+    // you can acces the what will display from the route/web 
+    // and choose what you want to see
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $a = "radwan";
+    $b = "mohamed";
+    // return view('welcome');
+    return view("index", ["tilte" => $a,]);
 });
